4ade89934c74b04e78b5fcc9a2eebd4b1bbacdcf
Bretton Wade <bretton.wade@gmail.com>
2018-03-11 22:18:51 -0400

    updates to the setup script

--- config/setup-raspberry-pi.sh
+++ config/setup-raspberry-pi.sh
@@ -6,7 +6,7 @@
 # get the name of the machine we are logging into
 RASPBERRY_PI=$1;
 if [[ -z  $RASPBERRY_PI  ]]; then
-    echo "Usage: $0 hostname <pi-password>";
+    echo "Usage: $0 <hostname> <password>";
     exit;
 fi
 
@@ -16,68 +16,80 @@ echo $PING_RASPBERRY_PI | grep "56 data bytes" &> /dev/null;
 if [ $? != 0 ]; then
     echo "Cannot connect to $RASPBERRY_PI";
     exit;
+else
+    echo "Found $RASPBERRY_PI";
+fi
+
+# check that we have sshpass
+SSHPASS_PATH=$(which sshpass)
+#echo "sshpass @ $SSHPASS_PATH";
+if [ ! -x "$SSHPASS_PATH" ]; then
+    echo "sshpass not found";
+    exit;
 fi
 
 # function to check that login works
 checkLogin() {
     local LOGIN_USER=$1;
     local LOGIN_PASSWORD=$2;
+    local LOGIN_MESSAGE=$3;
 
-    echo "Logging into $LOGIN_USER@$RASPBERRY_PI...";
+    echo "Logging into $LOGIN_USER@$RASPBERRY_PI ($LOGIN_MESSAGE)...";
     local LOGIN_RESPONSE="$(sshpass -p $LOGIN_PASSWORD ssh $LOGIN_USER@$RASPBERRY_PI printenv 2>&1)";
+    #echo "Login Response: $LOGIN_RESPONSE";
     echo $LOGIN_RESPONSE | grep "USER=$LOGIN_USER" &> /dev/null;
     if [ $? == 0 ]; then
         return 0;
-    else
-        echo "...Failed";
+    #else
+    #    echo "...Failed";
     fi
     return 1;
 }
 
 # set up a few defaults
 RASPBERRY_PI_USER="pi";
-RASPBERRY_PI_USER_PASSWORD=$2;
-# if the user didn't supply a password, we want to try to login with the defaults
-if [[ -z  $RASPBERRY_PI_USER_PASSWORD  ]]; then
-    RASPBERRY_PI_USER_PASSWORD="raspberry";
+RASPBERRY_PI_USER_PASSWORD="raspberry";
 
-    checkLogin $RASPBERRY_PI_USER $RASPBERRY_PI_USER_PASSWORD;
-    #echo "DEFAULT_LOGIN_RESPONSE=$DEFAULT_LOGIN_RESPONSE";
-    if [ $? == 0 ]; then
-        echo "Logged into $RASPBERRY_PI_USER@$RASPBERRY_PI with default credentials. We will now change the password to secure the box.";
-        NEW_RASPBERRY_PI_USER_PASSWORD="";
-        while [[ -z $NEW_RASPBERRY_PI_USER_PASSWORD ]]; do
-            echo "What would you like the new password to be?";
-            read NEW_RASPBERRY_PI_USER_PASSWORD;
-        done
-        # actually set the password
-        sshpass -p ${RASPBERRY_PI_USER_PASSWORD} ssh $RASPBERRY_PI_USER@$RASPBERRY_PI "echo $RASPBERRY_PI_USER:$NEW_RASPBERRY_PI_USER_PASSWORD | sudo chpasswd" 2>&1
-        # TODO might be nice to confirm the change worked
-        RASPBERRY_PI_USER_PASSWORD=$NEW_RASPBERRY_PI_USER_PASSWORD;
-    else
-        RASPBERRY_PI_USER_PASSWORD="";
-    fi
+# we want to try to login with the defaults
+checkLogin $RASPBERRY_PI_USER $RASPBERRY_PI_USER_PASSWORD "with default credentials";
+if [ $? == 0 ]; then
+    # get a new password for the box
+    echo "Change the default password to secure the box.";
+    NEW_RASPBERRY_PI_USER_PASSWORD=$2;
+    while [[ -z $NEW_RASPBERRY_PI_USER_PASSWORD ]]; do
+        echo "What would you like the new password to be?";
+        read NEW_RASPBERRY_PI_USER_PASSWORD;
+    done
+
+    # actually set the password
+    echo "Changing $RASPBERRY_PI_USER password to ($NEW_RASPBERRY_PI_USER_PASSWORD)";
+    sshpass -p ${RASPBERRY_PI_USER_PASSWORD} ssh $RASPBERRY_PI_USER@$RASPBERRY_PI "echo $RASPBERRY_PI_USER:$NEW_RASPBERRY_PI_USER_PASSWORD | sudo chpasswd" 2>&1
+    RASPBERRY_PI_USER_PASSWORD=$NEW_RASPBERRY_PI_USER_PASSWORD;
+else
+    RASPBERRY_PI_USER_PASSWORD=$2;
+    echo "Using $RASPBERRY_PI_USER password ($RASPBERRY_PI_USER_PASSWORD)";
 fi
 
 # function to check login with specific global variables
 checkLogin2() {
+    local LOGIN_MESSAGE=$1;
     if [[ ! -z $RASPBERRY_PI_USER_PASSWORD ]]; then
-        checkLogin $RASPBERRY_PI_USER $RASPBERRY_PI_USER_PASSWORD;
+        checkLogin $RASPBERRY_PI_USER $RASPBERRY_PI_USER_PASSWORD $LOGIN_MESSAGE;
         if [ $? != 0 ]; then
             RASPBERRY_PI_USER_PASSWORD="";
         fi
     fi
 }
 
-checkLogin2;
+checkLogin2 "verify credentials";
 while [[ -z $RASPBERRY_PI_USER_PASSWORD ]]; do
     echo "What is the password for $RASPBERRY_PI_USER@$RASPBERRY_PI? ";
     read RASPBERRY_PI_USER_PASSWORD;
-    checkLogin2;
+    checkLogin2 "retry";
 done
 
 # get a password to use for the new account
-echo "The next set of operations will create an account for $USER@$RASPBERRY_PI, if one doesn't exist.";
+echo "Next, create an account for $USER@$RASPBERRY_PI (if one doesn't exist).";
 echo "What is the password for this new user?";
 echo "(If you enter a blank password, a random one will be created)";
 read USER_PASSWORD;
@@ -89,20 +101,21 @@ if [[ -z $USER_PASSWORD ]]; then
     export LC_CTYPE=C
     USER_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1)
     export LC_CTYPE=$OLD_LC_CTYPE
-    echo "Created password for $USER ($USER_PASSWORD), make note of this for your records.";
+    echo "Created password for $USER ($USER_PASSWORD), make note of this for your records."
 fi
 
 # try to login with the supplied credentials
-checkLogin $USER $USER_PASSWORD
+checkLogin $USER $USER_PASSWORD "new user"
 if [ $? != 0 ]; then
-    echo "Creating new account...";
     # if that fails, create a new user and set the password
-    sshpass -p ${RASPBERRY_PI_USER_PASSWORD} ssh $RASPBERRY_PI_USER@$RASPBERRY_PI "sudo adduser $USER" 2>&1
+    echo "Creating new account...";
+    sshpass -p ${RASPBERRY_PI_USER_PASSWORD} ssh $RASPBERRY_PI_USER@$RASPBERRY_PI "sudo adduser $USER  --gecos 'First Last,RoomNumber,WorkPhone,HomePhone' --disabled-password" 2>&1
     sshpass -p ${RASPBERRY_PI_USER_PASSWORD} ssh $RASPBERRY_PI_USER@$RASPBERRY_PI "echo $USER:$USER_PASSWORD | sudo chpasswd" 2>&1
 
     # update sudoers so <me> can sudo without passwords
-    sshpass -p ${RASPBERRY_PI_USER_PASSWORD} ssh $RASPBERRY_PI_USER@$RASPBERRY_PI "sudo echo $USER ALL=(ALL) NOPASSWD: ALL >> /etc/sudoers" 2>&1
-
+    echo "Adding $USER to /etc/sudoers...";
+    # TODO don't do this if $USER is already in sudoers
+    sshpass -p ${RASPBERRY_PI_USER_PASSWORD} ssh $RASPBERRY_PI_USER@$RASPBERRY_PI "echo \"$USER ALL=(ALL) NOPASSWD: ALL\" | sudo tee -a /etc/sudoers" 2>&1
 else
     echo "Using existing account...";
 fi
@@ -115,10 +128,14 @@ if [ $? != 0 ]; then
     # copy the identity so I can login quietly from now on - we copy the keys too, so we can
     # use git quietly
     echo "Installing certs...";
-    sshpass -p $USER_PASSWORD ssh $RASPBERRY_PI mkdir -p -m 700 ~/.ssh;
+    sshpass -p $USER_PASSWORD ssh $RASPBERRY_PI "mkdir -p -m 700 .ssh";
+    echo "copy authorized keys.";
     sshpass -p $USER_PASSWORD scp ~/.ssh/id_rsa.pub $RASPBERRY_PI:~/.ssh/authorized_keys;
+    echo "copy id.";
     sshpass -p $USER_PASSWORD scp ~/.ssh/id_rsa $RASPBERRY_PI:~/.ssh/;
+    echo "copy pub.";
     sshpass -p $USER_PASSWORD scp ~/.ssh/id_rsa.pub $RASPBERRY_PI:~/.ssh/;
+    echo "Done installing certs.";
 else
     echo "Using existing certs...";
 fi
