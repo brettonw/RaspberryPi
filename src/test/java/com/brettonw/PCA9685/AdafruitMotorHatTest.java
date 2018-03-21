93622a3d9ea7337930fd2ae37526624b8e5ca4a0
Bretton Wade <brettonw.wade@gmail.com>
2018-03-21 01:17:41 -0400

    test all the motors

--- src/test/java/com/brettonw/PCA9685/AdafruitMotorHatTest.java
+++ src/test/java/com/brettonw/PCA9685/AdafruitMotorHatTest.java
@@ -1,6 +1,8 @@
 package com.brettonw.PCA9685;
 
 import com.brettonw.Utility;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 
 /**
@@ -10,28 +12,44 @@ import org.junit.Test;
  * or https://www.adafruit.com/product/711
  */
 public class AdafruitMotorHatTest {
+    protected static final Logger log = LogManager.getLogger (AdafruitMotorHatTest.class);
 
+    private AdafruitMotorHat motorHat;
+
+    public AdafruitMotorHatTest () {
+        log.debug ("CONSTRUCT");
+        motorHat = new AdafruitMotorHat ();
+    }
+
+    private void runMotor (AdafruitMotorHat.Motor motor) {
+        log.debug ("RUN (" + motor.name () + ")");
+        if (motorHat.hasDevice ()) {
+            log.debug ("FORWARD");
+            for (int i = 0; i <= PCA9685.CHANNEL_HIGH; i += 16) {
+                motorHat.runMotor (motor, i / 4095.0);
+                Utility.waitL (1);
+            }
+            Utility.waitD (5);
+            motorHat.stopMotor (motor);
+            Utility.waitD (1);
+
+            log.debug ("REVERSE");
+            for (int i = 0; i <= 4095; i += 16) {
+                motorHat.runMotor (motor, -i / 4095.0);
+                Utility.waitL (1);
+            }
+            Utility.waitD (5);
+            motorHat.stopMotor (motor);
+            Utility.waitD (1);
+        }
+    }
 
     @Test
     public void test1 () {
-        AdafruitMotorHat motorHat = new AdafruitMotorHat ();
-        if (motorHat.hasDevice ()) {
-            for (int i = 0; i <= PCA9685.CHANNEL_HIGH; i += 16) {
-                motorHat.runMotor (AdafruitMotorHat.Motor.MOTOR_1, i / 4095.0);
-                Utility.waitL (1);
-            }
-            Utility.waitD (5);
-            motorHat.stopMotor (AdafruitMotorHat.Motor.MOTOR_1);
-            Utility.waitD (1);
-
-            for (int i = 0; i <= 4095; i += 16) {
-                motorHat.runMotor (AdafruitMotorHat.Motor.MOTOR_1, -i / 4095.0);
-                Utility.waitL (1);
-            }
-            Utility.waitD (5);
-            motorHat.stopMotor (AdafruitMotorHat.Motor.MOTOR_1);
-            Utility.waitD (1);
-        }
+        runMotor (AdafruitMotorHat.Motor.MOTOR_1);
+        runMotor (AdafruitMotorHat.Motor.MOTOR_2);
+        runMotor (AdafruitMotorHat.Motor.MOTOR_3);
+        runMotor (AdafruitMotorHat.Motor.MOTOR_4);
     }
 
 }
