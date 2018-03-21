ca69d6ae7ee2a61baa0ff15ae208b4b05d6b00c5
Bretton Wade <brettonw.wade@gmail.com>
2018-03-21 00:11:34 -0400

    test fix

--- src/test/java/com/brettonw/PCA9685/AdafruitMotorHatTest.java
+++ src/test/java/com/brettonw/PCA9685/AdafruitMotorHatTest.java
@@ -1,5 +1,6 @@
 package com.brettonw.PCA9685;
 
+import com.brettonw.Utility;
 import org.junit.Test;
 
 /**
@@ -17,20 +18,19 @@ public class AdafruitMotorHatTest {
         if (motorHat.hasDevice ()) {
             for (int i = 0; i <= PCA9685.CHANNEL_HIGH; i += 16) {
                 motorHat.runMotor (AdafruitMotorHat.Motor.MOTOR_1, i / 4095.0);
-                motorHat.waitL (1);
+                Utility.waitL (1);
             }
-            motorHat.waitD (5);
+            Utility.waitD (5);
             motorHat.stopMotor (AdafruitMotorHat.Motor.MOTOR_1);
-            motorHat.waitD (1);
+            Utility.waitD (1);
 
             for (int i = 0; i <= 4095; i += 16) {
                 motorHat.runMotor (AdafruitMotorHat.Motor.MOTOR_1, -i / 4095.0);
-                motorHat.waitL (1);
+                Utility.waitL (1);
             }
-            motorHat.waitD (5);
+            Utility.waitD (5);
             motorHat.stopMotor (AdafruitMotorHat.Motor.MOTOR_1);
-            motorHat.waitL (1);
-            motorHat.waitD (1);
+            Utility.waitD (1);
         }
     }
 
